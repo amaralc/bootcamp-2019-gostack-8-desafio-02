d0a8a0c61812a6c6a3d126420acca3a99a14afac
amaralc <amaral.calil@gmail.com>
2019-09-05 00:27:19 -0300

    Criando loader de models: erro no teste do loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 // IMPORT
 import { Router } from 'express';
+import User from './app/models/User'; // temporário (lógica não ficará dentro das rotas, e sim no controller)
 
 // BODY
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'myName',
+    email: 'myname@email.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
 });
 
 // EXPORT
